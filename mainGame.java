ac52098b1b2572b9aacf69a9270a56cc3103e102
EnoughIsAhnaf <33615841+EnoughIsAhnaf@users.noreply.github.com>
2023-11-27 12:35:24 -0800

    Updated mainGame.java
    Started working on a prototype of the game through the console. No GUI is involve at the current moment.

--- mainGame.java
+++ mainGame.java
@@ -5,7 +5,8 @@ public class mainGame
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
-		System.out.println("Hello world!");
+		System.out.println("Hello and welcome to the game!");
+		System.out.println("Please select your three cards.");
 
 	}
 
